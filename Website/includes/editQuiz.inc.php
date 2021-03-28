917eee387a56bd2fac8c4e782cfc3a02f07796d5
Tamoor <48155649+vulpeeze@users.noreply.github.com>
2021-03-28 00:06:45 +0000

    Added bootstrap for edit quiz questions as I forgot to do that.

--- Website/includes/editQuiz.inc.php
+++ Website/includes/editQuiz.inc.php
@@ -11,24 +11,28 @@ if (isset($_GET["submit"])) {
     $sql = "SELECT * FROM quizmetadata WHERE quizId='" . $quizId . "';";
     $result = mysqli_query($conn, $sql);
 
+    include_once '../header.php';
+
     while ($row = mysqli_fetch_assoc($result)) {
-        echo "<h1>" .$row['quizName'] . "</h1>";
+        echo "<div class='container'><h1>" . $row['quizName'] . "</h1>";
     }
 
     echo '
+        <div class="row">
+        <div class="col-sm-6 text-start">
         <form action="../editQuizzes.php">
-            <input type="submit" value="Go back" />
-        </form>';
-    
-    echo '
+            <input type="submit" class="btn btn-primary" value="Go back" />
+        </form></div>';
+
+    echo '<div class="col-sm-6 text-end">
         <form action="deleteQuizzes.inc.php">
-            <button name="submit" value=' . $quizId . ' type="submit">Delete this Quiz</button>
-        </form>';
+            <button name="submit" class="btn btn-primary" value=' . $quizId . ' type="submit">Delete this Quiz</button>
+        </form></div></div>';
 
     $sql = "SELECT * FROM quizquestions WHERE quizId='" . $quizId . "';";
     $result = mysqli_query($conn, $sql);
 
-    echo "<table style='width:100%; border: 1px solid black;'>";
+    echo "<br><table class='table table-striped'>";
     echo "<tr>
         <th>Quiz Number</th>
         <th>Question</th>
@@ -42,32 +46,56 @@ if (isset($_GET["submit"])) {
     while ($row = mysqli_fetch_assoc($result)) {
         echo "<tr>
         <th>" . $quizNumbering . "</th>
-        <th>" .$row['questions'] . "</th>
-        <th>" .$row['answer1'] . "</th>
-        <th>" .$row['answer2'] . "</th>
-        <th>" .$row['answer3'] . "</th>
-        <th>" .$row['answer4'] . "</th>
-        <th>" .$row['rightAnswer'] . "</th>
+        <th>" . $row['questions'] . "</th>
+        <th>" . $row['answer1'] . "</th>
+        <th>" . $row['answer2'] . "</th>
+        <th>" . $row['answer3'] . "</th>
+        <th>" . $row['answer4'] . "</th>
+        <th>" . $row['rightAnswer'] . "</th>
         </tr>";
         $quizNumbering++;
     }
     echo "</table>";
 
-    echo '<h2>Add Question</h2>
+    echo '
+    <h2>Add Question</h2>
     <form action="addQuestion.inc.php" method="post">
-        <label for="question">Question:</label>
-        <input type="text" id="question" name="question"><br>
-        <label for="ans1">Answer 1:</label>
-        <input type="text" id="ans1" name="ans1"><br>
-        <label for="ans2">Answer 2:</label>
-        <input type="text" id="ans2" name="ans2"><br>
-        <label for="ans3">Answer 3:</label>
-        <input type="text" id="ans3" name="ans3"><br>
-        <label for="ans4">Answer 4:</label>
-        <input type="text" id="ans4" name="ans4"><br>
-        <label for="ansr">Which is the right answer:</label>
-        <input type="number" id="ansr" name="ansr"><br>
-        <br><br><input type="submit" name="submit" value="Add Question">
+    <div class="form-group row">
+        <label for="question" class="col-sm-3 col-form-label">Question:</label>
+        <div class="col-sm-9">
+        <input type="text" class="form-control" id="question" name="question"><br>
+        </div>
+    </div>
+    <div class="form-group row">
+        <label for="ans1" class="col-sm-3 col-form-label">Answer 1:</label>
+        <div class="col-sm-9">
+        <input type="text" class="form-control" id="ans1" name="ans1"><br>
+        </div>
+    </div>
+    <div class="form-group row">
+        <label for="ans2" class="col-sm-3 col-form-label">Answer 2:</label>
+        <div class="col-sm-9">
+        <input type="text" class="form-control" id="ans2" name="ans2"><br>
+        </div>
+    </div>
+    <div class="form-group row">
+        <label for="ans3" class="col-sm-3 col-form-label">Answer 3:</label>
+        <div class="col-sm-9">
+        <input type="text" class="form-control" id="ans3" name="ans3"><br>
+        </div>
+    </div>
+    <div class="form-group row">
+        <label for="ans4" class="col-sm-3 col-form-label">Answer 4:</label>
+        <div class="col-sm-9">
+        <input type="text" class="form-control" id="ans4" name="ans4"><br>
+        </div>
+    </div>
+    <div class="form-group row">
+        <label for="ansr" class="col-sm-3 col-form-label">Which is the right answer:</label>
+        <div class="col-sm-6">
+        <input type="number" class="form-control" id="ansr" name="ansr">
+        </div>
+        <input type="submit" class="btn btn-primary col-sm-3" name="submit" value="Add Question">
     </form>';
     if (isset($_GET["error"])) {
         if ($_GET["error"] == "emptyInput") {
@@ -78,12 +106,18 @@ if (isset($_GET["submit"])) {
             echo "<p>Something went wrong.</p>";
         }
     }
+    echo "</div><hr>";
+
 
     echo '<h2>Delete Question</h2>
     <form action="deleteQuestion.inc.php" method="post">
-        <label for="delq">Which question to delete:</label>
-        <input type="number" id="delq" name="delq"><br>
-        <br><br><input type="submit" name="submit" value="Delete Question">
+    <div class="form-group row">
+        <label for="delq" class="col-sm-3 col-form-label">Which question to delete:</label>
+        <div class="col-sm-6">
+        <input type="number" class="form-control" id="delq" name="delq">
+        </div>
+        <input type="submit" class="btn btn-primary col-sm-3" name="submit" value="Delete Question">
+    </div>
     </form>';
     if (isset($_GET["Delerror"])) {
         if ($_GET["Delerror"] == "emptyInput") {
@@ -94,24 +128,54 @@ if (isset($_GET["submit"])) {
             echo "<p>Something went wrong.</p>";
         }
     }
+    echo "<hr>";
 
     echo '<h2>Update Question</h2>
+    <h5>Choose a question to update and anything left blank will be kept the same.</h5><br>
     <form action="updateQuestion.inc.php" method="post">
-        <label for="upq">Which question to update:</label>
-        <input type="number" id="upq" name="upq"><br>
-        <label for="question">Question:</label>
-        <input type="text" id="question" name="question"><br>
-        <label for="ans1">Answer 1:</label>
-        <input type="text" id="ans1" name="ans1"><br>
-        <label for="ans2">Answer 2:</label>
-        <input type="text" id="ans2" name="ans2"><br>
-        <label for="ans3">Answer 3:</label>
-        <input type="text" id="ans3" name="ans3"><br>
-        <label for="ans4">Answer 4:</label>
-        <input type="text" id="ans4" name="ans4"><br>
-        <label for="ansr">Which is the right answer:</label>
-        <input type="number" id="ansr" name="ansr"><br>
-        <br><br><input type="submit" name="submit" value="Update Question">
+    <div class="form-group row">
+        <label for="upq" class="col-sm-3 col-form-label">Which question to update:</label>
+        <div class="col-sm-9">
+        <input type="number" class="form-control" id="upq" name="upq"><br>
+        </div>
+    </div>
+    <div class="form-group row">
+        <label for="question" class="col-sm-3 col-form-label">Question:</label>
+        <div class="col-sm-9">
+        <input type="text" class="form-control" id="question" name="question"><br>
+        </div>
+    </div>
+    <div class="form-group row">
+        <label for="ans1" class="col-sm-3 col-form-label">Answer 1:</label>
+        <div class="col-sm-9">
+        <input type="text" class="form-control" id="ans1" name="ans1"><br>
+        </div>
+    </div>
+    <div class="form-group row">
+        <label for="ans2" class="col-sm-3 col-form-label">Answer 2:</label>
+        <div class="col-sm-9">
+        <input type="text" class="form-control" id="ans2" name="ans2"><br>
+        </div>
+    </div>
+    <div class="form-group row">
+        <label for="ans3" class="col-sm-3 col-form-label">Answer 3:</label>
+        <div class="col-sm-9">
+        <input type="text" class="form-control" id="ans3" name="ans3"><br>
+        </div>
+    </div>
+    <div class="form-group row">
+        <label for="ans4" class="col-sm-3 col-form-label">Answer 4:</label>
+        <div class="col-sm-9">
+        <input type="text" class="form-control" id="ans4" name="ans4"><br>
+        </div>
+    </div>
+    <div class="form-group row">
+        <label for="ansr" class="col-sm-3 col-form-label">Which is the right answer:</label>
+        <div class="col-sm-6">
+        <input type="number" class="form-control" id="ansr" name="ansr">
+        </div>
+        <input type="submit" class="btn btn-primary col-sm-3" name="submit" value="Update Question">
+    </div>
     </form>';
     if (isset($_GET["uperror"])) {
         if ($_GET["uperror"] == "emptyInput") {
@@ -124,7 +188,7 @@ if (isset($_GET["submit"])) {
             echo "<p>Something went wrong.</p>";
         }
     }
-
+    echo "</div>";
 } else {
     //header("location: ../editQuizzes.php?error=failedToEdit");
     exit();
